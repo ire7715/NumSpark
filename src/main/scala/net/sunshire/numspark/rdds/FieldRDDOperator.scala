beb4191c9247c82c374c09afe466b4b4e1ed8d02
ire_sun <ire7715@hotmail.com>
2017-06-22 16:46:30 +0800

    reduceFields method and its test case

--- src/main/scala/net/sunshire/numspark/rdds/FieldRDDOperator.scala
+++ src/main/scala/net/sunshire/numspark/rdds/FieldRDDOperator.scala
@@ -18,6 +18,31 @@ object FieldRDDOperator {
     return sourceDataframe.join(newFieldDataframe, keyName).rdd;
   }
 
+  def reduceFields(
+      sourceRDD: RDD[Row],
+      key: String = "",
+      inputParams: Array[String] = Array[String](),
+      preservedParams: Array[String] = Array[String]())
+      (reduce: RDD[Row] => RDD[Row]): RDD[Row] = {
+    val sc = sourceRDD.sparkContext;
+    val sqlContext = SQLContext.getOrCreate(sc);
+    val sourceSchema = sourceRDD.first.schema;
+    val sourceDataframe = sqlContext.createDataFrame(sourceRDD, sourceSchema);
+    val inputRDD =
+      if (inputParams.length == 0) sourceRDD
+      else sourceDataframe.select(key, inputParams: _*).rdd;
+    val reducedRDD = reduce(inputRDD);
+    val reducedDataframe = sqlContext.createDataFrame(
+      reducedRDD, reducedRDD.first.schema);
+
+
+    if (preservedParams.length == 0)
+      return reducedDataframe.rdd;
+    else
+      return reducedDataframe.join(
+        sourceDataframe.select(key, preservedParams: _*), key).rdd;
+  }
+
   implicit def RDD2FieldRDDOperator(rdd: RDD[Row]) = new FieldRDDOperator(rdd);
 }
 
@@ -25,4 +50,11 @@ class FieldRDDOperator(sourceRDD: RDD[Row]) {
   private[rdds] def getRDD() = sourceRDD;
 
   def newField(map: RDD[Row] => RDD[Row]) = FieldRDDOperator.newField(sourceRDD) (map);
+
+  def reduceFields(
+      key: String = "",
+      inputParams: Array[String] = Array[String](),
+      preservedParams: Array[String] = Array[String]())
+      (reduce: RDD[Row] => RDD[Row]): RDD[Row] =
+    FieldRDDOperator.reduceFields(sourceRDD, key, inputParams, preservedParams) (reduce);
 }
--- src/test/scala/net/sunshire/numspark/rdds/FieldRDDOperatorSuite.scala
+++ src/test/scala/net/sunshire/numspark/rdds/FieldRDDOperatorSuite.scala
@@ -3,6 +3,7 @@ package net.sunshire.numspark.rdds;
 import com.holdenkarau.spark.testing.SharedSparkContext;
 import net.sunshire.numspark.rdds.FieldRDDOperator._;
 import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Row, SQLContext};
 import org.apache.spark.sql.types.{StructType, StructField};
 import org.apache.spark.sql.types.IntegerType;
@@ -100,7 +101,110 @@ class FieldRDDOperatorSuite
     for(pair <- expectedArray.zip(newFieldArray)) {
       assert(pair._1.getAs[Integer](0) == pair._2.getAs[Integer]("valueA"));
       assert(pair._1.getAs[Integer](1) == pair._2.getAs[Integer]("valueB"));
-      assert(pair._1.getAs[Integer](2) == pair._2.getAs[Integer]("valueC"));
+      assert(pair._1.getAs[Integer](2) == pair._2.getAs[Integer]("valueZ"));
+    }
+  }
+
+  test("static reduce fields w/ input and preservation") {
+    val expectedSchema = StructType(Array(
+      StructField("valueA", IntegerType, false),
+      StructField("valueBC", IntegerType, false),
+      StructField("valueD", IntegerType, false)
+    ));
+    val expectedArray = data.map(v => Row(v, v * 5 + v * 7, v * 11));
+
+    val reducedFieldsArray = FieldRDDOperator.reduceFields(
+        dataRDD, "valueA", Array("valueB", "valueC"), Array("valueD")) { inputRDD =>
+      val sc = inputRDD.sparkContext;
+      val sqlContext = new SQLContext(sc);
+      import sqlContext.implicits._;
+      val inputDataframe = sqlContext.createDataFrame(inputRDD, inputRDD.first.schema);
+      inputDataframe.select(
+        $"valueA", expr("valueB + valueC").alias("valueBC")).rdd;
+    }.collect.sortBy(_.getAs[Integer]("valueA"));
+
+    for (pair <- expectedSchema.fields.zip(reducedFieldsArray(0).schema.fields))
+      assert(pair._1 == pair._2)
+    for (pair <- expectedArray.zip(reducedFieldsArray)) {
+      assert(pair._1.getAs[Integer](0) == pair._2.getAs[Integer]("valueA"));
+      assert(pair._1.getAs[Integer](1) == pair._2.getAs[Integer]("valueBC"));
+      assert(pair._1.getAs[Integer](2) == pair._2.getAs[Integer]("valueD"));
+    }
+  }
+
+  test("static reduce fields w/o input and preservation") {
+    val expectedSchema = StructType(Array(
+      StructField("valueA", IntegerType, false),
+      StructField("valueBC", IntegerType, false)
+    ));
+    val expectedArray = data.map(v => Row(v, v * 5 + v * 7));
+
+    val reducedFieldsArray = FieldRDDOperator.reduceFields(
+        dataRDD) { inputRDD =>
+      val sc = inputRDD.sparkContext;
+      val sqlContext = new SQLContext(sc);
+      import sqlContext.implicits._;
+      val inputDataframe = sqlContext.createDataFrame(inputRDD, inputRDD.first.schema);
+      inputDataframe.select(
+        $"valueA", expr("valueB + valueC").alias("valueBC")).rdd;
+    }.collect.sortBy(_.getAs[Integer]("valueA"));
+
+    for (pair <- expectedSchema.fields.zip(reducedFieldsArray(0).schema.fields))
+      assert(pair._1 == pair._2)
+    for (pair <- expectedArray.zip(reducedFieldsArray)) {
+      assert(pair._1.getAs[Integer](0) == pair._2.getAs[Integer]("valueA"));
+      assert(pair._1.getAs[Integer](1) == pair._2.getAs[Integer]("valueBC"));
+    }
+  }
+
+  test("instantiated reduce fields w/ input and preservation") {
+    val expectedSchema = StructType(Array(
+      StructField("valueA", IntegerType, false),
+      StructField("valueBC", IntegerType, false),
+      StructField("valueD", IntegerType, false)
+    ));
+    val expectedArray = data.map(v => Row(v, v * 5 + v * 7, v * 11));
+
+    val reducedFieldsArray = dataRDD.reduceFields(
+        "valueA", Array("valueB", "valueC"), Array("valueD")) { inputRDD =>
+      val sc = inputRDD.sparkContext;
+      val sqlContext = new SQLContext(sc);
+      import sqlContext.implicits._;
+      val inputDataframe = sqlContext.createDataFrame(inputRDD, inputRDD.first.schema);
+      inputDataframe.select(
+        $"valueA", expr("valueB + valueC").alias("valueBC")).rdd;
+    }.collect.sortBy(_.getAs[Integer]("valueA"));
+
+    for (pair <- expectedSchema.fields.zip(reducedFieldsArray(0).schema.fields))
+      assert(pair._1 == pair._2)
+    for (pair <- expectedArray.zip(reducedFieldsArray)) {
+      assert(pair._1.getAs[Integer](0) == pair._2.getAs[Integer]("valueA"));
+      assert(pair._1.getAs[Integer](1) == pair._2.getAs[Integer]("valueBC"));
+      assert(pair._1.getAs[Integer](2) == pair._2.getAs[Integer]("valueD"));
+    }
+  }
+
+  test("instantiated reduce fields w/o input and preservation") {
+    val expectedSchema = StructType(Array(
+      StructField("valueA", IntegerType, false),
+      StructField("valueBC", IntegerType, false)
+    ));
+    val expectedArray = data.map(v => Row(v, v * 5 + v * 7));
+
+    val reducedFieldsArray = dataRDD.reduceFields() { inputRDD =>
+      val sc = inputRDD.sparkContext;
+      val sqlContext = new SQLContext(sc);
+      import sqlContext.implicits._;
+      val inputDataframe = sqlContext.createDataFrame(inputRDD, inputRDD.first.schema);
+      inputDataframe.select(
+        $"valueA", expr("valueB + valueC").alias("valueBC")).rdd;
+    }.collect.sortBy(_.getAs[Integer]("valueA"));
+
+    for (pair <- expectedSchema.fields.zip(reducedFieldsArray(0).schema.fields))
+      assert(pair._1 == pair._2)
+    for (pair <- expectedArray.zip(reducedFieldsArray)) {
+      assert(pair._1.getAs[Integer](0) == pair._2.getAs[Integer]("valueA"));
+      assert(pair._1.getAs[Integer](1) == pair._2.getAs[Integer]("valueBC"));
     }
   }
 }
