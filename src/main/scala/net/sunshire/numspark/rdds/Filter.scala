74bac268cea976c6a7b62f7fe7d7808a867ca694
ire_sun <ire7715@hotmail.com>
2017-06-27 15:27:32 +0800

    adding scaladocs for Filter methods

--- src/main/scala/net/sunshire/numspark/rdds/Filter.scala
+++ src/main/scala/net/sunshire/numspark/rdds/Filter.scala
@@ -6,6 +6,14 @@ import org.apache.spark.sql.Row;
 import scala.reflect.ClassTag;
 
 object Filter {
+  /**
+    * Filter an RDD by another RDD and the given key.
+    *
+    * @param sourceRDD: RDD[Row]; The RDD which is going to be filtered.
+    * @param key: String; The filtering key name.
+    * @param rdd: RDD[Row]; The RDD which contains only the filtering constraints.
+    * @return RDD[Row]; The RDD after filtered.
+    */
   def inRDD[K](sourceRDD: RDD[Row], key: String, rdd: RDD[K])
   (implicit kt: ClassTag[K], ord: Ordering[K] = null): RDD[Row] = {
     val keyedRDD = rdd.distinct.map(value => (value, null));
@@ -18,6 +26,14 @@ object Filter {
     return filteredRDD;
   }
 
+  /**
+    * Filter an RDD by an array and the given key.
+    *
+    * @param sourceRDD: RDD[Row]; The RDD which is going to be filtered.
+    * @param key: String; The filtering key name.
+    * @param array: Array; The array which contains only the filtering constraints.
+    * @return RDD[Row]; The RDD after filtered.
+    */
   def inArray[K](sourceRDD: RDD[Row], key: String, array: Array[K])
   (implicit kt: ClassTag[K]): RDD[Row] = {
     val sc = sourceRDD.context;
@@ -27,16 +43,36 @@ object Filter {
     return filteredRDD;
   }
 
+  /**
+    * Implicitly converts RDD to Filter class
+    *
+    * @param rdd: RDD[Row]; the source RDD.
+    * @return Filter
+    */
   implicit def RDD2Filter(rdd: RDD[Row]) = new Filter(rdd);
 }
 
 class Filter(sourceRDD: RDD[Row]) {
   private[rdds] def getRDD = sourceRDD;
 
+  /**
+    * Filter this RDD by another RDD and the given key.
+    *
+    * @param key: String; The filtering key name.
+    * @param rdd: RDD[Row]; The RDD which contains only the filtering constraints.
+    * @return RDD[Row]; The RDD after filtered.
+    */
   def inRDD[K](key: String, rdd: RDD[K])
   (implicit kt: ClassTag[K], ord: Ordering[K] = null): RDD[Row] =
     Filter.inRDD(sourceRDD, key, rdd);
 
+  /**
+    * Filter this RDD by an array and the given key.
+    *
+    * @param key: String; The filtering key name.
+    * @param array: Array; The array which contains only the filtering constraints.
+    * @return RDD[Row]; The RDD after filtered.
+    */
   def inArray[K](key: String, rdd: Array[K])
   (implicit kt: ClassTag[K]): RDD[Row] = Filter.inArray(sourceRDD, key, rdd);
 }
